2fa3279f2aba8f875a3af1d45f3771804593ec04
TheHarmanCodes <168176533+TheHarmanCodes@users.noreply.github.com>
2026-09-15 18:19:42 +0530

    Setup Auth Layout, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,31 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constant/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center ">
+      <h1 className="font-bold text-2xl font-space-grotesk">
+        Welcome to Ultimate Next.js
+      </h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit" className="cursor-pointer">
+          Log out
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center ">
-      <h1 className="font-bold text-2xl font-space-grotesk">
-        Welcome to Ultimate Next.js
-      </h1>
-    </div>
-  );
-}
